f82cbb7b17914df41cad74add96ebdaab97583e9
radioshiny <radioshiny@gmail.com>
2023-08-07 14:45:42 +0900

    init .readthedocs.yaml

--- docs/conf.py
+++ docs/conf.py
@@ -13,15 +13,28 @@ author = 'Shinyoung Kim'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
+html_theme_options = {
+    'navigation_depth': 5,
+}
