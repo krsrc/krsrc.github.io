f4dde7f6ffef71bcdeac79e49d28fa4278355f6e
radioshiny <radioshiny@gmail.com>
2023-08-07 15:53:51 +0900

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,9 +32,9 @@ pygments_style = 'sphinx'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-import sphinx_rtd_theme
+# import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = ["_themes",]
 html_static_path = ['_static']
 html_theme_options = {
     'navigation_depth': 5,
